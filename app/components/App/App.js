c2f77b3c82da0b09011db1eb044a8a72b0d58e5a
Kostya Podlesniy <konstantin-921@yandex.ru>
2018-08-14 14:32:46 +0300

    Fix sharing for non-authorizing users

--- app/components/App/App.js
+++ app/components/App/App.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import MainPage from '../MainPage';
 
-const App = () => {
+const App = (props) => {
   const token = localStorage.getItem('token.id');
   const user = localStorage.getItem('user.id');
-  const startPage = (token && user && user !== 'undefined' && token !== 'undefined') ? <Route path="/" component={MainPage} /> : <Redirect to="/login" />;
+  const str = props.location.pathname;
+  let startPage = '';
+  if (str.includes('share')) {
+    startPage = <Route path="/" component={MainPage} />;
+    const token = props.location.search.substring(19);
+    localStorage['token.id'] = token;
+  } else {
+    startPage = (token && user && user !== 'undefined' && token !== 'undefined') ? <Route path="/" component={MainPage} /> : <Redirect to="/login" />;
+  }
   return (
     <div>
       {startPage}
